59502deb412d5530d17508669f27b58af6e52404
adercv <aderc19@gmail.com>
2020-09-15 16:36:16 -0400

    First Routes and First View Creation

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::post('/register', function (){
+    return 'Registro de Usuário';
+});
+
+Route::get('/empresa', function (){
+    return '<h1 style = "color: red; ">Sobre a empresa</h1>';
+});
+
+Route::get('/contato', function (){
+    return view('contact');
+});
 
 Route::get('/', function () {
     return view('welcome');
